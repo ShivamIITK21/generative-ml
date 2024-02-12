a688e1ecd8f841e095f9963839ed551459809125
Shivam <sshivam21@iitk.ac.in>
2024-02-13 02:10:28 +0530

    added some git stuff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from distributions.bernoulli import Bernoulli
+import numpy as np
 
 if __name__ == "__main__":
+    a = np.array([1,2,3])
     b = Bernoulli([0.5, 0.1, 0.4])
     print(b.sample())
--- visual_utils/visualizer.py
+++ visual_utils/visualizer.py
@@ -1,4 +1,23 @@
+import base64
+import requests
 
 class Visualizer:
     def __init__(self):
-        self.url = "http"
+        self.add_url = "http://localhost:8080/add"
+        self.remove_url = "http://localhost:8080/remove"
+
+    @staticmethod
+    def toB64(s: str):
+        return base64.b64encode(s.encode("ascii"))
+
+    def draw(self, id: str, exp: str):
+        requests.get(url =self.add_url, params={"id": self.toB64(id), "exp": self.toB64(exp)})
+
+    def remove(self, id: str):
+        requests.get(url=self.remove_url, params={"id": self.toB64(id)})
+
+    @staticmethod
+    def getLatexKV(key: float, val: float) -> str:
+        return r"""\{0 \le y <""" + f"""{val}""" r"""\}"""+ r"""x=""" + f"""{key}"""
+
+
